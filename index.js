da39a7424f4ddfa87b905528b7fe15bc4b7b32a8
Aleksandre <gabeditebuli@gmail.com>
2023-02-03 17:05:58 +0400

    slight visual updates

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const LETTERS = new Array(26)
-  .fill()
-  .map((el, ind) => String.fromCharCode(65 + ind));
+const LETTERS = new Array(26).fill().map((el, ind) => String.fromCharCode(65 + ind));
 
 const COLLECTION = [
   "CLASS",
@@ -10,15 +8,60 @@ const COLLECTION = [
   "PSEUDO-CLASS",
   "GLOBAL",
 ];
+
 let counter = 0;
 let hangManHealth = 7;
+let playButton = document.getElementById("play-button");
+let hiddenWord = document.getElementById("hidden-word");
+let scene = document.getElementById("hang-bodzi");
+
+let guessWord = generateWord(COLLECTION);
+populateKeyboard(LETTERS);
+hideWord(guessWord);
+
+// hiddenWord.style.left = `${250 - hiddenWord.offsetWidth / 2}px`;
+
+playButton.addEventListener("click", () => {
+  playButton.style.display = "none";
+  keyboard.style.display = "flex";
+  hiddenWord.style.display = "flex";
+  hiddenWord.style.bottom = `${10 + keyboard.offsetHeight + 30}px`;
+  
+  //   scene.style.display = "block";
+});
+
+function generateButton(letter) {
+  let btn = document.createElement("button");
+  btn.innerText = letter;
+  btn.setAttribute("id", letter);
+  btn.style.width = "45px";
+  btn.style.fontSize = "45px";
+  btn.contentEditable = true;
+  btn.addEventListener("click", buttonClickHandler);
+  btn.addEventListener("keydown", e => {
+    console.log(this)
+    btn.disabled = true;
+  });
+  return btn;
+};
 
 function populateKeyboard(letters) {
   let keyboard = document.getElementById("keyboard");
   letters.forEach((el) => keyboard.appendChild(generateButton(el)));
-}
-populateKeyboard(LETTERS);
-let playButton = document.getElementById("play-button");
+};
+
+function reverseHypen(word, letter) {
+  let activeWord = document.getElementById("hidden-word").innerText;
+  let pattern = activeWord.split(/_\\-/gi).join("").trim();
+  let searchPattern = new RegExp(`[^(${letter}${pattern}\\-)]`, "gi");
+  let reversedWord = word.replace(searchPattern, "_");
+  hiddenWord.innerText = reversedWord;
+};
+
+function hideWord(word) {
+  let hypenedWord = word.replace(/[a-z]/gi, "_");
+  hiddenWord.innerText = hypenedWord;
+};
 
 function generateWord(arr) {
   let result;
@@ -30,93 +73,67 @@ function generateWord(arr) {
     counter = 1;
   }
   return result;
-}
+};
 
-let hiddenWord = document.getElementById("hidden-word");
-let guessWord = generateWord(COLLECTION);
-let scene = document.getElementById("hang-bodzi");
-hideWord(guessWord);
+function buttonClickHandler (e)  {
+  this.disabled = true;
+  if (guessWord.includes(this.innerText)) {
+    reverseHypen(guessWord, this.innerText);
+  } else if (!guessWord.includes(this.innerText) && hangManHealth > 0) {
+    console.log(hangManHealth);
 
-// hiddenWord.style.left = `${250 - hiddenWord.offsetWidth / 2}px`;
-
-function hideWord(word) {
-  let hypenedWord = word.replace(/[a-z]/gi, "_");
-  hiddenWord.innerText = hypenedWord;
-}
-function reverseHypen(word, letter) {
-  let activeWord = document.getElementById("hidden-word").innerText;
-  let pattern = activeWord.split(/_\\-/gi).join("").trim();
-  let searchPattern = new RegExp(`[^(${letter}${pattern}\\-)]`, "gi");
-  let reversedWord = word.replace(searchPattern, "_");
-  hiddenWord.innerText = reversedWord;
-}
-
-function generateButton(letter) {
-  let btn = document.createElement("button");
-  btn.innerText = letter;
-  btn.setAttribute("id", letter);
-  btn.style.width = "15px";
-  btn.addEventListener("click", (e) => {
-    btn.disabled = true;
-    if (guessWord.includes(btn.innerText)) {
-      reverseHypen(guessWord, btn.innerText);
-    } else if (!guessWord.includes(btn.innerText) && hangManHealth > 0) {
-      console.log(hangManHealth);
-
-      switch (hangManHealth) {
-        case 7:
-          document.getElementById("hang-bodzi").style.display = "block";
-          document.getElementById("floor").style.display = "inline-block";
-          break;
-        case 6:
-          document.getElementById("hang2").style.display = "inline-block";
-          document.getElementById("hang3").style.display = "inline-block";
-          break;
-        case 5:
-          document.getElementById("hang4").style.display = "inline-block";
-          break;
-        case 4:
-          document.getElementById("leg").style.display = "inline-block";
-          document.getElementById("leg1").style.display = "inline-block";
-          break;
-        case 3:
-          document.getElementById("body").style.display = "inline-block";
-          break;
-        case 2:
-          document.getElementById("hand").style.display = "inline-block";
-          document.getElementById("hand1").style.display = "inline-block";
-          break;
-        case 1:
-          document.getElementById("head").style.display = "inline-block";
-          break;
-      }
-      hangManHealth--;
-    } else if (hangManHealth === 0) {
-      let gameOver = document.getElementById("game-container");
-      gameOver.innerText = "GAME OVER!";
-    } else {
-      throw new Error("Hanged man got Health issues!");
+    switch (hangManHealth) {
+      case 7:
+        document.getElementById("hang-bodzi").style.display = "block";
+        document.getElementById("floor").style.display = "inline-block";
+        break;
+      case 6:
+        document.getElementById("hang2").style.display = "inline-block";
+        document.getElementById("hang3").style.display = "inline-block";
+        break;
+      case 5:
+        document.getElementById("hang4").style.display = "inline-block";
+        break;
+      case 4:
+        document.getElementById("leg").style.display = "inline-block";
+        document.getElementById("leg1").style.display = "inline-block";
+        break;
+      case 3:
+        document.getElementById("body").style.display = "inline-block";
+        break;
+      case 2:
+        document.getElementById("hand").style.display = "inline-block";
+        document.getElementById("hand1").style.display = "inline-block";
+        break;
+      case 1:
+        document.getElementById("head").style.display = "inline-block";
+        break;
     }
-    console.log(btn.innerText, guessWord, guessWord.includes(btn.innerText));
-    if (!hiddenWord.innerText.includes("_")) {
-      guessWord = generateWord(COLLECTION);
-      hideWord(guessWord);
-      LETTERS.forEach((el) => {
-        let resetBtn = document.getElementById(el);
-        resetBtn.disabled = false;
-      });
-    }
-  });
-  return btn;
+    hangManHealth--;
+  } else if (hangManHealth === 0) {
+    let gameOver = document.getElementById("game-container");
+    gameOver.classList.add('game-over')
+    gameOver.innerText = "GAME OVER!";
+  } else {
+    throw new Error("Hanged man got Health issues!");
+  }
+  console.log(this.innerText, guessWord, guessWord.includes(this.innerText));
+  if (!hiddenWord.innerText.includes("_")) {
+    guessWord = generateWord(COLLECTION);
+    hideWord(guessWord);
+    LETTERS.forEach((el) => {
+      let resetBtn = document.getElementById(el);
+      resetBtn.disabled = false;
+    });
+  }
 }
 
-playButton.addEventListener("click", () => {
-  playButton.style.display = "none";
-  keyboard.style.display = "flex";
-  hiddenWord.style.display = "inline-block";
-  hiddenWord.style.bottom = `${10 + keyboard.offsetHeight + 30}px`;
-  hiddenWord.style.left = `${250 - hiddenWord.offsetWidth / 2}px`;
-  //   scene.style.display = "block";
-});
+
+
+
+
+
 
 function dagecaMekhi() {}
+
+document.addEventListener('keydown', e => console.log(e.key))
