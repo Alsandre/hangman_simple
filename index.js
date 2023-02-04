63797f423035182070c1b82c82e26aae39cd63cc
Aleksandre <gabeditebuli@gmail.com>
2023-02-04 20:26:49 +0400

    added letters animation

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const LETTERS = new Array(26).fill().map((el, ind) => String.fromCharCode(65 + ind));
+const LETTERS = new Array(26)
+  .fill()
+  .map((el, ind) => String.fromCharCode(65 + ind));
 
 const COLLECTION = [
   "CLASS",
@@ -15,9 +17,10 @@ let playButton = document.getElementById("play-button");
 let hiddenWord = document.getElementById("hidden-word");
 let scene = document.getElementById("hang-bodzi");
 
-let guessWord = generateWord(COLLECTION);
+let guessWordArray = generateWord(COLLECTION);
+let revealWordArray = [...guessWordArray];
 populateKeyboard(LETTERS);
-hideWord(guessWord);
+hideWord(guessWordArray);
 
 // hiddenWord.style.left = `${250 - hiddenWord.offsetWidth / 2}px`;
 
@@ -26,7 +29,7 @@ playButton.addEventListener("click", () => {
   keyboard.style.display = "flex";
   hiddenWord.style.display = "flex";
   hiddenWord.style.bottom = `${10 + keyboard.offsetHeight + 30}px`;
-  
+
   //   scene.style.display = "block";
 });
 
@@ -38,48 +41,63 @@ function generateButton(letter) {
   btn.style.fontSize = "45px";
   btn.contentEditable = true;
   btn.addEventListener("click", buttonClickHandler);
-  btn.addEventListener("keydown", e => {
-    console.log(this)
+  btn.addEventListener("keydown", (e) => {
+    console.log(this);
     btn.disabled = true;
   });
   return btn;
-};
+}
 
 function populateKeyboard(letters) {
   let keyboard = document.getElementById("keyboard");
   letters.forEach((el) => keyboard.appendChild(generateButton(el)));
-};
+}
 
-function reverseHypen(word, letter) {
-  let activeWord = document.getElementById("hidden-word").innerText;
-  let pattern = activeWord.split(/_\\-/gi).join("").trim();
-  let searchPattern = new RegExp(`[^(${letter}${pattern}\\-)]`, "gi");
-  let reversedWord = word.replace(searchPattern, "_");
-  hiddenWord.innerText = reversedWord;
-};
+function revealWord(word, letter) {
+  // let tempArr = word.split("");
+  word.forEach((el, ind) => {
+    if (el === letter) {
+      let boxToReveal = document.getElementById(
+        `${word.join("")}_${ind}_${letter}`
+      );
+      boxToReveal.innerText = letter;
+    }
+  });
+  revealWordArray = [...revealWordArray.filter((el) => el !== letter)];
+  console.log(revealWordArray);
+}
 
 function hideWord(word) {
-  let hypenedWord = word.replace(/[a-z]/gi, "_");
-  hiddenWord.innerText = hypenedWord;
-};
+  // let hypenedWord = word.replace(/[a-z]/gi, "_");
+  // hiddenWord.innerText = hypenedWord;
+
+  // let wordLetters = [...word];
+  word.forEach((el, ind) => {
+    let letterBox = document.createElement("div");
+    // letterBox.innerText = el;
+    letterBox.classList.add("hidden-letter");
+    letterBox.id = `${word.join("")}_${ind}_${el}`;
+    hiddenWord.appendChild(letterBox);
+  });
+}
 
 function generateWord(arr) {
   let result;
   if (counter < COLLECTION.length) {
-    result = arr[counter];
+    result = arr[counter].split("");
     counter++;
   } else {
     result = arr[0];
     counter = 1;
   }
   return result;
-};
+}
 
-function buttonClickHandler (e)  {
+function buttonClickHandler(e) {
   this.disabled = true;
-  if (guessWord.includes(this.innerText)) {
-    reverseHypen(guessWord, this.innerText);
-  } else if (!guessWord.includes(this.innerText) && hangManHealth > 0) {
+  if (guessWordArray.includes(this.innerText)) {
+    revealWord(guessWordArray, this.innerText);
+  } else if (!guessWordArray.includes(this.innerText) && hangManHealth > 0) {
     console.log(hangManHealth);
 
     switch (hangManHealth) {
@@ -112,28 +130,38 @@ function buttonClickHandler (e)  {
     hangManHealth--;
   } else if (hangManHealth === 0) {
     let gameOver = document.getElementById("game-container");
-    gameOver.classList.add('game-over')
+    gameOver.classList.add("game-over");
     gameOver.innerText = "GAME OVER!";
   } else {
     throw new Error("Hanged man got Health issues!");
   }
-  console.log(this.innerText, guessWord, guessWord.includes(this.innerText));
-  if (!hiddenWord.innerText.includes("_")) {
-    guessWord = generateWord(COLLECTION);
-    hideWord(guessWord);
-    LETTERS.forEach((el) => {
-      let resetBtn = document.getElementById(el);
-      resetBtn.disabled = false;
-    });
+
+  if (revealWordArray.length === 0) {
+    let lettersCollection = document.getElementsByClassName("hidden-letter");
+    let i = 0;
+    for (i; i < lettersCollection.length; i++) {
+      lettersCollection[i].style.animationName = "fade-out";
+      lettersCollection[i].style.animationDelay = `${i * 1}s`;
+    }
+    setTimeout(() => {
+      hiddenWord.innerText = guessWordArray.join("");
+    }, i * 1000);
+    setTimeout(() => {
+      hiddenWord.innerHTML = "";
+    }, i * 1490);
+    setTimeout(() => {
+      guessWordArray = generateWord(COLLECTION);
+      hideWord(guessWordArray);
+      revealWordArray = [...guessWordArray];
+      LETTERS.forEach((el) => {
+        let resetBtn = document.getElementById(el);
+        resetBtn.disabled = false;
+      });
+    }, i * 1500);
+    console.log(guessWordArray);
   }
 }
 
-
-
-
-
-
-
 function dagecaMekhi() {}
 
-document.addEventListener('keydown', e => console.log(e.key))
+document.addEventListener("keydown", (e) => console.log(e.key));
